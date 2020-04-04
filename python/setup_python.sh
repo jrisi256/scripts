131a8ac2d3d845f03318616f2a469e18261d6f4c
jrisi256 <jrisi256@gmail.com>
2020-04-04 15:04:12 -0400

    Changed documentation to reflect how to run setup_python.sh. Also am testing out some changes to the setup script.

--- python/setup_python.sh
+++ python/setup_python.sh
@@ -8,17 +8,16 @@ sudo apt install bzip2 curl
 # Download and install Miniconda
 curl -O https://repo.anaconda.com/miniconda/Miniconda3-py38_4.8.2-Linux-x86_64.sh
 bash Miniconda3-py38_4.8.2-Linux-x86_64.sh -p $1
-conda config --set auto_activate_base false
 rm Miniconda3-py38_4.8.2-Linux-x86_64.sh
 
 # Add Python (and related commands) to our PATH
 touch python.sh
 echo PATH=$1/bin:'$PATH' >> python.sh
 sudo mv python.sh /etc/profile.d/
-source /etc/profile.d/python.sh
+# source /etc/profile.d/python.sh # This doesn't propagate up to our current shell environment
 
-# Install Jupyter, make sure pip is pointing to the version of pip just installed
-pip install jupyter jupyterlab
+# Turn off conda automatically activating its environment
+# conda config --set auto_activate_base false
 
-# Install virtualenv
-pip install virtualenv
+# Install Jupyter and virtualenv, make sure pip is pointing to the version of pip just installed
+# pip install jupyter jupyterlab virtualenv
