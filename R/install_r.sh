fbece8ca2c20e21ab0d6dd654f70821cd642476f
jrisi256 <jrisi256@gmail.com>
2020-09-30 19:13:21 -0400

    Added more system-level packges to install_r.sh so R packages will work.

--- R/install_r.sh
+++ R/install_r.sh
@@ -8,6 +8,9 @@ sudo apt install curl gdebi-core
 # Necessary for curl, openssl package in R
 sudo apt install libcurl4-openssl-dev libssl-dev
 
+# Necessary for xml2, systemfonts, and gdtools packages in R which are used in flextable
+sudo apt install libxml2-dev libfontconfig1-dev libcairo2-dev
+
 # Download and install the prepackaged binaries
 curl -O https://cdn.rstudio.com/r/ubuntu-1804/pkgs/r-$1_1_amd64.deb
 sudo gdebi r-$1_1_amd64.deb
