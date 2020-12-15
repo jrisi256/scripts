6c59eda31192f72ee0265638482c46cfbd28e632
jrisi256 <jrisi256@gmail.com>
2020-12-14 20:10:35 -0500

    Downloaded new version of RStudio

--- R/install_rstudio.sh
+++ R/install_rstudio.sh
@@ -4,6 +4,6 @@
 
 # Install Rstudio, I install the .deb because it's easier than downloading the tarball (which would require configure/make)
 sudo apt install gdebi-core
-wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.959-amd64.deb
-sudo gdebi rstudio-1.3.959-amd64.deb
-rm rstudio-1.3.959-amd64.deb
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-1.3.1093-amd64.deb
+sudo gdebi rstudio-1.3.1093-amd64.deb
+rm rstudio-1.3.1093-amd64.deb
