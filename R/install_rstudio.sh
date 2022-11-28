3b6d978b34ef673c15533106fe6d3e15746e8621
jrisi256 <jrisi256@gmail.com>
2022-11-28 17:11:22 -0500

    Added new version of RStudio.

--- R/install_rstudio.sh
+++ R/install_rstudio.sh
@@ -4,6 +4,6 @@
 
 # Install Rstudio, I install the .deb because it's easier than downloading the tarball (which would require configure/make)
 sudo apt install gdebi-core
-wget https://download1.rstudio.org/desktop/jammy/amd64/rstudio-2022.07.1-554-amd64.deb
-sudo gdebi rstudio-2022.07.1-554-amd64.deb
-rm rstudio-2022.07.1-554-amd64.deb
+wget https://download1.rstudio.org/desktop/bionic/amd64/rstudio-2022.07.2-576-amd64.deb
+sudo gdebi rstudio-2022.07.2-576-amd64.deb
+rm rstudio-2022.07.2-576-amd64.deb
