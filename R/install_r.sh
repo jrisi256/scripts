7d8f755a1bff06feb9e14187e1a38ccd43c0d563
jrisi256 <jrisi256@gmail.com>
2023-02-20 03:30:59 -0500

    No longer install R packages.

--- R/install_r.sh
+++ R/install_r.sh
@@ -12,5 +12,7 @@ rm r-$2_1_amd64.deb
 sudo ln -sf /opt/R/$2/bin/R /usr/local/bin/R
 sudo ln -sf /opt/R/$2/bin/Rscript /usr/local/bin/Rscript
 
-# Install R Packages which are used across all R projects
-R -e 'install.packages(c("devtools", "renv", "markdown", "rmarkdown", "lintr"), repos = "http://cran.us.r-project.org")'
+# Install R Packages which are used across all R projects.
+# renv ignores the user's directory so I will comment this out.
+# Useful to have around because I always install these packages for every project I work on.
+# R -e 'install.packages(c("devtools", "renv", "markdown", "rmarkdown", "lintr"), repos = "http://cran.us.r-project.org")'
