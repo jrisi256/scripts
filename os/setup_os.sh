8ace3e1dba369228052b1dadc8cac6fad4beb666
jrisi256 <jrisi256@gmail.com>
2023-06-07 00:09:17 -0400

    Got rid of some extra whitespace.

--- os/setup_os.sh
+++ os/setup_os.sh
@@ -117,4 +117,3 @@ Pin-Priority: 1001
 echo 'Unattended-Upgrade::Allowed-Origins:: "LP-PPA-mozillateam:${distro_codename}";' | sudo tee /etc/apt/apt.conf.d/51unattended-upgrades-firefox
 
 sudo apt install firefox
-
